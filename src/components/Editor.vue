40aa2b4e7b1dbaa6e9964106fbb5f7e1bff72d13
mimikun <dzdzble_Effort_311@outlook.jp>
2018-05-01 01:07:26 +0900

    色々fixした

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -9,7 +9,7 @@
       </div>
       <button class="addMemoBtn" @click="addMemo">メモの追加</button>
       <button class="deleteMemoBtn" v-if="memos.length > 1" @click="deleteMemo">選択中のメモの削除</button>
-      <button class="savaMemoBtn" @click="saveMemos">メモの保存</button>
+      <button class="savaMemosBtn" @click="saveMemos">メモの保存</button>
     </div>
       <textarea class="markdown" v-model="memos[selectedIndex].markdown"></textarea>
       <div class="preview" v-html="preview()"></div>
@@ -32,14 +32,14 @@ export default {
   },
   created: function() {
     firebaes
-    .database()
-    .ref('memos' + this.user.uid)
-    .once('value')
-    .then(result => {
-      if (result.val()) {
-        this.memos = result.val()
-      }
-    })
+      .database()
+      .ref('memos/' + this.user.uid)
+      .once('value')
+      .then(result => {
+        if (result.val()) {
+          this.memos = result.val()
+        }
+      })
   },
   methods: {
     logout: function() {
@@ -58,9 +58,9 @@ export default {
     },
     saveMemos: function() {
       firebase
-      .database
-      .ref('memos/' + this.user.uid)
-      .set(this.memos);
+        .database
+        .ref('memos/' + this.user.uid)
+        .set(this.memos);
     },
     selectMemo: function(index) {
       this.selectedIndex = index;
